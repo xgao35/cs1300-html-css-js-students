16967c4a85b1b3682fbabe8e586cf230ba98769b
xgao35 <xing_gao@brown.edu>
2022-09-10 18:04:07 -0400

    finished studio

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -29,7 +29,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+  console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -40,15 +40,21 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
-
+  const filteredArray = fruitsArray.filter((fruit) => fruit.nutritions.sugar > 15);
+  console.log(filteredArray);
   // TODO: Create a new HTML element to display your data 
-
+  
   // TODO: Append your new element to the page
-
+  // Create a new HTML element and set its properties
+  filteredArray.forEach((fruit) => {
+    const newFruit = document.createElement('div');
+    newFruit.innerHTML = fruit.name;
+    gallery.append(newFruit)
+  })
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
